58aaab7d4de40dbcc76ab4f9b8b02dede070a493
lpasfiel <lpasfiel@as.edu.au>
2018-06-05 08:19:07 +1000

    Finished the Warning

--- src/salsaboy/salsaengine/ui/Warning.java
+++ src/salsaboy/salsaengine/ui/Warning.java
@@ -6,16 +6,20 @@ import java.awt.*;
 public class Warning extends JFrame {   //TODO Finish this
     private JLabel label = new JLabel();
     private JButton ok = new JButton("OK");
+    private void onClick() {
+        dispose();
+    }
     public Warning(String message) {
         setLayout(null);
         
         label.setHorizontalAlignment(SwingConstants.CENTER);
         label.setText(message);
-        label.setBounds(0, 0, 300, 50);
+        label.setBounds(0, 0, 300, 60);
         add(label);
         
         ok.setSize(100, 30);
         ok.setLocation(100, 50);
+        ok.addActionListener(e -> onClick());
         add(ok);
     
         setSize(300, 100);
