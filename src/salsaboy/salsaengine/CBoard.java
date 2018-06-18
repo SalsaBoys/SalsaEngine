3d3e96f2c58184312e704c2336e9cf5b093d8890
lpasfiel <lpasfiel@as.edu.au>
2018-06-18 21:09:30 +1000

    Read the clipboard

--- src/salsaboy/salsaengine/CBoard.java
+++ src/salsaboy/salsaengine/CBoard.java
@@ -2,9 +2,25 @@ package salsaboy.salsaengine;
 
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
 
 public class CBoard {
+    public static String get() {
+        String result = "";
+        try {
+            Toolkit toolkit = Toolkit.getDefaultToolkit();
+            Clipboard clipboard = toolkit.getSystemClipboard();
+            result = (String) clipboard.getData(DataFlavor.stringFlavor);
+        } catch (UnsupportedFlavorException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
     public static void save(String message) {
         StringSelection stringSelection = new StringSelection(message);
         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
