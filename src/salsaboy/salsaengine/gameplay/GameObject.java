f1d98afa743e0e141a3eebe0df62aa9614dd5de6
TheOnlyMrCat <signupforforums5@gmail.com>
2018-06-28 18:02:06 +1000

    Initialized positional settings in GameObject

--- src/salsaboy/salsaengine/gameplay/GameObject.java
+++ src/salsaboy/salsaengine/gameplay/GameObject.java
@@ -1,4 +1,12 @@
 package salsaboy.salsaengine.gameplay;
 
 public class GameObject {
+
+	protected int x, y, z; // Z can be render order for now
+
+	public GameObject(int x, int y, int z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
 }
