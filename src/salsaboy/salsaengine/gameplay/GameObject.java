9f1b00be376b37b49a084366b6ec42210893bacd
lpasfiel <lpasfiel@as.edu.au>
2018-06-30 13:25:09 +1000

    Just an OCD ritual. Might be a few in the future. Don't worry.

--- src/salsaboy/salsaengine/gameplay/GameObject.java
+++ src/salsaboy/salsaengine/gameplay/GameObject.java
@@ -1,8 +1,7 @@
 package salsaboy.salsaengine.gameplay;
 
 public class GameObject {
-
-	protected int x, y, z; // Z can be render order for now
+	protected int x, y, z; //Z can be render order for now
 
 	public GameObject(int x, int y, int z) {
 		this.x = x;
