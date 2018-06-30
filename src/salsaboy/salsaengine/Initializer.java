d0fb88b0df234a0f20d571b58860f82f9f96540d
lpasfiel <lpasfiel@as.edu.au>
2018-06-30 14:22:46 +1000

    Added some documentation and a few tweaks to the program.

--- src/salsaboy/salsaengine/Initializer.java
+++ src/salsaboy/salsaengine/Initializer.java
@@ -3,7 +3,6 @@ package salsaboy.salsaengine;
 import salsaboy.salsaengine.gameplay.GameType;
 
 public interface Initializer {
-    void start();
     /**
      * This is so that you can change the type through the game
      * @return the type
